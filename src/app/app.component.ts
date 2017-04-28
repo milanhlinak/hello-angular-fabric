6a2837dbb79d557a88cb90fe21f1734fb4b4ebd0
Milan Hlinák <hlinakmilan91@gmail.com>
2017-04-28 22:04:12 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import 'fabric';
+declare const fabric: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+
+  private canvas;
+
+  ngOnInit() {
+
+    this.canvas = new fabric.Canvas('canvas', {
+      width: 300,
+      height: 300
+    });
+
+    this.canvas.add(new fabric.Rect({
+      left: 100,
+      top: 100,
+      width: 50,
+      height: 50,
+      fill: '#faa'
+    }));
+
+  }
+
 }
